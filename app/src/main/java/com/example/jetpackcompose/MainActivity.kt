dc6fdc5eab2a69620cb48c049c717481c59b339a
luana-vargas <vargas.luana@outlook.com.br>
2020-03-25 18:23:30 -0300

    Inserido imagens

--- app/src/main/java/com/example/jetpackcompose/MainActivity.kt
+++ app/src/main/java/com/example/jetpackcompose/MainActivity.kt
@@ -1,11 +1,21 @@
 package com.example.jetpackcompose
 
+import android.content.res.Resources
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.Composable
-import androidx.ui.core.Text
-import androidx.ui.core.setContent
+import androidx.compose.unaryPlus
+import androidx.core.content.res.ResourcesCompat
+import androidx.ui.core.*
+import androidx.ui.foundation.DrawImage
+import androidx.ui.foundation.shape.corner.CircleShape
+import androidx.ui.graphics.Color
+import androidx.ui.graphics.Image
+import androidx.ui.graphics.imageFromResource
+import androidx.ui.layout.*
 import androidx.ui.material.MaterialTheme
+import androidx.ui.material.themeTextStyle
+import androidx.ui.res.imageResource
 import androidx.ui.tooling.preview.Preview
 
 class MainActivity : AppCompatActivity() {
@@ -13,11 +23,47 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             MaterialTheme {
-                Greeting("Android")
+                Root()
             }
         }
     }
 }
+
+@Composable
+fun Root(){
+    //alinhamento vertical
+    Column(crossAxisSize = LayoutSize.Expand,
+            mainAxisSize = LayoutSize.Wrap) {
+
+        //tamanho imagem
+        Container(expanded = true, height = 200.dp) {
+            DrawImage(+imageResource(R.drawable.tanlan))
+
+        }
+
+            Row(){
+                Padding(padding = 8.dp) {
+                Container(width = 32.dp, height = 32.dp) {
+
+                    Clip(shape = CircleShape) {
+                        DrawImage(+imageResource(R.drawable.tanlan2))
+                    }
+                }
+
+                }
+                
+                Column (modifier = Spacing(8.dp)) {
+                    Text(text = "Hello, Android!",
+                            style = +themeTextStyle { subtitle1 } )
+                    Text(text = "Hello, World!",
+                            style = +themeTextStyle { subtitle2.copy(color = Color.Gray) })
+                    
+                }
+            }
+        }
+    }
+
+
 
 @Composable
 fun Greeting(name: String) {
@@ -31,3 +77,5 @@ fun DefaultPreview() {
         Greeting("Android")
     }
 }
+
+// 42 min de video
